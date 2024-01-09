6c89f4dabc6fbcb0b5a18194c8bcf40aafb1dfc9
GvozdevIM <ilyagvozdev@mail.ru>
2024-01-09 22:34:28 +0300

    add seminars folders

--- Seminars/sem1/Program.cs
+++ Seminars/sem1/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿// System.Console.WriteLine("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(num);
+
+// Напишите программу, которая на вход
+// принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+
+// System.Console.WriteLine("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num2 * num2 == num1)
+// {
+//     System.Console.WriteLine($"{num1} is quad of {num2}");
+// }
+// else
+// {
+//     System.Console.WriteLine($"{num1} is not quad of {num2}");
+// }
+
+
+
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+
+// System.Console.WriteLine("Input N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = -n; i <=n; i++)
+// {
+//     System.Console.Write(i + " ");
+// }
+
+
+// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+// Console.WriteLine("Input number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int first = n / 100;
+// int third = n % 10;
+// Console.WriteLine($"Сумма 1ой и 3ий цифры в числе {n} = {first+third}");
+
