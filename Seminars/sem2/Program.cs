2f0121a5756288bb581330e944e1c430a648525e
GvozdevIM <ilyagvozdev@mail.ru>
2024-01-14 19:21:10 +0300

    add  Seminars:sem2

---
+++ Seminars/sem2/Program.cs
@@ -0,0 +1,22 @@
+﻿// void numToQuad(int num)
+// {
+//     System.Console.WriteLine(num * num);
+// }
+
+// System.Console.WriteLine("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// numToQuad(number);
+
+int numToQuad(int num)
+{
+    return num * num;
+}
+
+System.Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// int result = numToQuad(number);
+// System.Console.WriteLine(result);
+
+System.Console.WriteLine(numToQuad(number));
