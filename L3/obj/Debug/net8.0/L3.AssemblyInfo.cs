328e9b9be2bdb775549661872f10f183ced4641a
GvozdevIM <ilyagvozdev@mail.ru>
2024-01-16 22:34:23 +0300

    add  lesson3:task1

--- L3/obj/Debug/net8.0/L3.AssemblyInfo.cs
+++ L3/obj/Debug/net8.0/L3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("homeworks2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("L3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("homeworks2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("homeworks2")]
+[assembly: System.Reflection.AssemblyProductAttribute("L3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("L3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
