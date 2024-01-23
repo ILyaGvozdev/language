67c94f79451354e6d4e2f7c416fb9186e80dfd4b
GvozdevIM <ilyagvozdev@mail.ru>
2024-01-23 23:35:54 +0300

    add Lesson4:task1

---
+++ L4/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int[,] CreateMatrix(int columnsCount, int rowsCount)
+{
+    int[,] matrix = new int[columnsCount,rowsCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(1,10);
+        }
+    }
+    return matrix;
+}
+
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(4,5);
+ShowMatrix(matrix);
