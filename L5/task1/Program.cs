43db442751b48959445a77fe7fb0547489bef48a
GvozdevIM <ilyagvozdev@mail.ru>
2024-01-29 22:22:23 +0300

    add Lesson5;Sem5

---
+++ L5/task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int Fact(int n)
+{
+    int sum = 1;
+    if(n==1 || n==0) return 1;
+    System.Console.WriteLine($"{n}");
+    int fa = n * Fact(n-1);
+    System.Console.WriteLine(fa);
+    return fa;
+    
+}
+
+System.Console.Write(Fact(5));
+ 
