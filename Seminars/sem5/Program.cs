2372f92c4a71be74dfefc6c76dee7dc2f5d39f79
GvozdevIM <ilyagvozdev@mail.ru>
2024-01-30 21:10:38 +0300

    add Sem5++

--- Seminars/sem5/Program.cs
+++ Seminars/sem5/Program.cs
@@ -14,4 +14,114 @@
 int[,] CreateMatrix(int rows, int columns, int min, int max)
 {
     int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
 }
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Find(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = matrix[i, j] * matrix[i, j];
+            }
+        }
+    }
+}
+
+// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
+// Пример
+// 2 3 4 3
+// 4 3 4 1 => 2 + 3 + 5 = 10
+// 2 9 5 4
+
+int EqualityOfMatrixRC(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+
+// Задайте двумерный массив из целых чисел. Сформируйте новый
+// одномерный массив, состоящий из средних арифметических
+// значений по строкам двумерного массива.
+// Пример
+// 2 3 4 3
+// 4 3 4 1 => [3 3 5]
+// 2 9 5 4
+
+double[] SrArArray(int[,] matrix)
+{
+    int sum = 0;
+    double[] srArray = new double[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum+=matrix[i,j];
+        }
+        srArray[i] = sum / (double) matrix.GetLength(1);
+        sum = 0;
+    }
+    return srArray;
+}
+
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+
+
+System.Console.WriteLine("Input num of rows");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input num of columns");
+int columns = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input minimal value");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input maximal value");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] array = CreateMatrix(rows, columns, min, max);
+PrintMatrix(array);
+System.Console.WriteLine();
+PrintArray(SrArArray(array));
